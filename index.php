abd5d2d42e2fa417226a1dd5d9a975808993cf7d
EnricoClem <enrico.clem94@gmail.com>
2024-05-09 17:05:49 +0200

    ADD - array generation inide class

--- index.php
+++ index.php
@@ -5,16 +5,73 @@ class Production {
     public $language;
     public $rate;
 
+    public static $allProduction = array();
+
     public function __construct($_title, $_language, $_rate) {
         $this->title = $_title;
         $this->language = $_language;
         $this->rate = $_rate;
+        self::$allProduction[] = $this;
     }
+
+    public function __toString(){
+        return $this->title;
+        return $this->language;
+        return $this->rate;
+    }
+
 }
 
-$lotr = new Production("Lord of the Rings", "English", 10);
+$lotr = new Production("Lord of the Rings", "english", 10);
 
-$castAwayMoon = new Production("Cast Away on the Moon", "Korean", 7);
+$castAwayMoon = new Production("Cast Away on the Moon", "korean", 7);
 
-var_dump($lotr);
-var_dump($castAwayMoon);
+$ladriBiciclette = new Production("Ladri di biciclette", "italian", 8);
+
+$theRoom = new Production("The Roon", "english", 2);
+
+$druk = new Production("Druk", "danish", 9);
+
+$fastAndFuriousTD = new Production("Fast & Furious: Tokyo Drift", "english", 6);
+
+var_dump(Production::$allProduction);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <div class="bg-info">
+            <div class="container p-3 text-light">
+                <h1>Production list</h1>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <ul class="row">
+                <?php
+                for($i = 0; $i < count(Production); $i++){
+                    ?>
+                    <li class="col-3">
+                        <h3><?php echo $title; ?></h3>
+                        <p><?php echo $language; ?></p>
+                        <p><?php echo $rate; ?></p>
+                    </li>
+                    <?php
+                }
+                ?>
+            </ul>
+        </div>
+    </main>
+</body>
+</html>
