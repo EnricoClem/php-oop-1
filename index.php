6d5af9e4a57bf688155d5b9173583d2355f15497
EnricoClem <enrico.clem94@gmail.com>
2024-05-09 15:40:58 +0200

    ADD - two object movie and var dump test

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
 class Production {
-    public title;
-    public language;
-    public rate;
+    public $title;
+    public $language;
+    public $rate;
 }
+
+$lotr = new Production();
+$lotr->title = "Lord of the Rings";
+$lotr->language = "English";
+$lotr->rate = 10;
+
+$castAwayMoon = new Production();
+$castAwayMoon->title = "Cast Away on the Moon";
+$castAwayMoon->language = "Korean";
+$castAwayMoon->rate = 7;
+
+var_dump($lotr);
+var_dump($castAwayMoon);
